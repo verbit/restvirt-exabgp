9844118c810ad7ce2b8acbfd6e1b79af68ba3b4d
Ilya Verbitskiy <ilya@verbit.io>
2021-07-18 20:53:41 +0200

    Use restvirt's route API

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.16
 
 require (
 	github.com/tidwall/gjson v1.8.0
-	github.com/verbit/restvirt-client v0.3.0
+	github.com/verbit/restvirt-client v0.4.0
 )
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/verbit/restvirt-client"
 )
 
-type dummy struct {}
+type dummy struct{}
 type StringSet map[string]dummy
 
 func NewStringSet() StringSet {
@@ -24,7 +24,7 @@ func (s StringSet) append(v string) {
 
 func (s StringSet) String() string {
 	keys := make([]string, 0, len(s))
-	for key, _ := range s {
+	for key := range s {
 		keys = append(keys, key)
 	}
 	return "[" + strings.Join(keys, " ") + "]"
@@ -41,6 +41,68 @@ func setDefault(m map[string]StringSet, key string, value StringSet) StringSet {
 	return value
 }
 
+var namespace = "bgp"
+
+func update(client *restvirt.Client, j *gjson.Result) {
+	w := j.Get("neighbor.message.update.withdraw.ipv4 unicast.#.nlri")
+	for _, source := range w.Array() {
+		fmt.Fprintf(os.Stderr, "- %v\n", source)
+		delete(nexthop, source.String())
+	}
+	r := j.Get("neighbor.message.update.announce.ipv4 unicast")
+	for target, result := range r.Map() {
+		for _, source := range result.Get("#.nlri").Array() {
+			fmt.Fprintf(os.Stderr, "+ %v -> %v\n", source, target)
+			targets := setDefault(nexthop, source.String(), NewStringSet())
+			targets.append(target)
+		}
+	}
+	fmt.Fprintf(os.Stderr, "= %v\n", nexthop)
+
+	routes, err := client.GetRoutesInNamespace(namespace)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, route := range routes {
+		if _, ok := nexthop[route.Destination]; !ok {
+			err := client.DeleteRoute(strings.ReplaceAll(route.Destination, "/", "-"))
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+	}
+
+	for dest, hops := range nexthop {
+		gateways := make([]string, 0, len(hops))
+		for gateway := range hops {
+			gateways = append(gateways, gateway)
+		}
+		err := client.SetRoute(restvirt.Route{
+			Destination: dest,
+			Namespace:   namespace,
+			Gateways:    gateways,
+		})
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
+
+func removeAll(client *restvirt.Client) {
+	routes, err := client.GetRoutesInNamespace(namespace)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, route := range routes {
+		err := client.DeleteRoute(strings.ReplaceAll(route.Destination, "/", "-"))
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
+
 func main() {
 	client, err := restvirt.NewClientFromEnvironment()
 	if err != nil {
@@ -52,24 +114,11 @@ func main() {
 		line := scanner.Text()
 		fmt.Fprintln(os.Stderr, line)
 		j := gjson.Parse(line)
-		if j.Get("type").String() != "update" {
-			continue
+		switch {
+		case j.Get("type").String() == "update":
+			update(client, &j)
+		case j.Get("type").String() == "notification" && j.Get("notification").String() == "shutdown":
+			removeAll(client)
 		}
-		w := j.Get("neighbor.message.update.withdraw.ipv4 unicast.#.nlri")
-		for _, source := range w.Array() {
-			fmt.Fprintf(os.Stderr, "- %v\n", source)
-			delete(nexthop, source.String())
-		}
-		r := j.Get("neighbor.message.update.announce.ipv4 unicast")
-		for target, result := range r.Map() {
-			for _, source := range result.Get("#.nlri").Array() {
-				fmt.Fprintf(os.Stderr, "+ %v -> %v\n", source, target)
-				targets := setDefault(nexthop, source.String(), NewStringSet())
-				targets.append(target)
-			}
-		}
-		fmt.Fprintf(os.Stderr, "= %v\n", nexthop)
 	}
-
-	_ = client
 }
